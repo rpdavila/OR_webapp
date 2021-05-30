32cebbbda26c86108cf9849f855b26995d6bfa6c
rpdavila <rafael.pietri@gmail.com>
2021-05-30 18:17:03 -0400

    revised social media links on home page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Footer.css'
-import Facebook from '../images/SocialMedia/f_logo_RGB-Blue_58.png'
-import instagram from '../images/SocialMedia/Instagram_Glyph_Gradient_RGB.png'
-import u_tube from '../images/SocialMedia/youtube_social_circle_red.png'
+// import Facebook from '../images/SocialMedia/f_logo_RGB-Blue_58.png'
+// import instagram from '../images/SocialMedia/Instagram_Glyph_Gradient_RGB.png'
+import u_tube from '../images/SocialMedia/youtube_social_squircle_red.png'
+import linked_in from "../images/SocialMedia/linkedin.png"
 
 
 const Footer = () => {
     return (
         <div className={`footer`}>
             <div className={'sm-container'}>
-                <img className={'f-social'} src={Facebook} alt={'facebook'}/>
-                <img className={'insta-social'} src={instagram} alt={'instagram'}/>
+                {/*<img className={'f-social'} src={Facebook} alt={'facebook'}/>*/}
+                {/*<img className={'insta-social'} src={instagram} alt={'instagram'}/>*/}
                 <img className={'u-tube-social'} src={u_tube} alt={'youtube'}/>
+                <img className={'linked-in'} src={linked_in} alt={'linked in'}/>
             </div>
             <div className={'cr-container'}>
                 &copy; Copyright 2021 Dr. Orlando Ruiz
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Image from "./IMG-3118.jpg"
 import "./Home.css"
-import Facebook from "../images/SocialMedia/f_logo_RGB-Blue_58.png";
-import instagram from "../images/SocialMedia/Instagram_Glyph_Gradient_RGB.png";
-import u_tube from "../images/SocialMedia/youtube_social_circle_red.png";
+// import Facebook from "../images/SocialMedia/f_logo_RGB-Blue_58.png";
+// import instagram from "../images/SocialMedia/Instagram_Glyph_Gradient_RGB.png";
+import u_tube from "../images/SocialMedia/youtube_social_squircle_red.png";
 import linked_in from "../images/SocialMedia/linkedin.png"
+
+
 const Home = () => {
     return (
         <div className={'container'}>
@@ -15,7 +17,7 @@ const Home = () => {
                     {/*<img className={'f-social'} src={Facebook} alt={'facebook'}/>*/}
                     {/*<img className={'insta-social'} src={instagram} alt={'instagram'}/>*/}
                     <img className={'u-tube-social'} src={u_tube} alt={'youtube'}/>
-                    <img className={'linked-in'} src={linked_in} alt={'linked in'}/>
+                    <img className={'linked-in-home'} src={linked_in} alt={'linked in'}/>
                 </div>
             </div>
             <div className={'photo-container'}>
