46419a400f9ccf266f4b2a19b8c5e270e372f0a8
rpdavila <rafael.pietri@gmail.com>
2021-07-01 14:52:54 -0400

    added relative path to img element in about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../images/chair.jpg"
+import image from "../../../src/components/images/chair.jpg"
 import bphoto from "../images/Paint.jpg"
 import './About.css'
 
