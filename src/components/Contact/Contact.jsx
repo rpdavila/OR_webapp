2218c20fe085794af1fc598c518375ba5af4970c
rpdavila <rafael.pietri@gmail.com>
2021-07-04 14:36:21 -0400

    amplify integration

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
 
     const submitData = (data, token) => {
         // call a backend API to verify reCAPTCHA response
-        fetch('http://localhost:3000/verify', {
+        fetch('https://iqils27xok.execute-api.us-east-1.amazonaws.com/staging', {
           method: 'POST',
           headers: {
             "Content-Type": "application/json"
