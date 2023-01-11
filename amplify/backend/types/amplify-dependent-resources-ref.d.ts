e42245d4198b257753f1c976ab1587781f86bec7
rafael Pietri-Davila <rafael.pietri@gmail.com>
2023-01-11 16:07:18 +0100

    updated amplify api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "orwebapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "function": {
+    "contactformuploader": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
